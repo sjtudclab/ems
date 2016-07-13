ebbce9051223c4e5afa1e7d2fa9f62fe4bf1c584
alvis <347390728@qq.com>
2016-07-13 21:55:33 +0800

    model definition completed

--- src/org/dclab/model/MatchingBean.java
+++ src/org/dclab/model/MatchingBean.java
@@ -1,5 +1,45 @@
 package org.dclab.model;
 
-public class MatchingBean {
+import java.io.Serializable;
+import java.util.List;
 
+/**
+ * 匹配题对象：
+ * 1.题目内容及id；
+ * 2.是否需要检查；
+ * 3.包含考生答案id的list
+ * @author alvis
+ *
+ */
+public class MatchingBean implements Serializable {
+	private int id;//题目id
+	private String content;//题目内容
+	private boolean ifCheck;//是否需要检查
+	private List<?> choiceIdList;//考生答案id的list
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public boolean isIfCheck() {
+		return ifCheck;
+	}
+	public void setIfCheck(boolean ifCheck) {
+		this.ifCheck = ifCheck;
+	}
+	public List<?> getChoiceIdList() {
+		return choiceIdList;
+	}
+	public void setChoiceIdList(List<?> choiceIdList) {
+		this.choiceIdList = choiceIdList;
+	}
+	
 }
--- src/org/dclab/model/MultiChoicesBean.java
+++ src/org/dclab/model/MultiChoicesBean.java
@@ -1,5 +1,47 @@
 package org.dclab.model;
 
-public class MultiChoicesBean {
+import java.io.Serializable;
+import java.util.*;
 
+/**
+ * 多选题对象：
+ * 1.题干内容以及id。
+ * 2.包含考生答案id的list。
+ * 3.是否需要检查
+ * @author alvis
+ *
+ */
+public class MultiChoicesBean implements Serializable {
+	
+	private int id;//题目id
+	private String content;//题目内容
+	private boolean ifCheck;//是否需要检查
+	private List<?> choiceIdList;//考生答案id的list
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public boolean isIfCheck() {
+		return ifCheck;
+	}
+	public void setIfCheck(boolean ifCheck) {
+		this.ifCheck = ifCheck;
+	}
+	public List<?> getChoiceId() {
+		return choiceIdList;
+	}
+	public void setChoiceId(List<?> choiceId) {
+		this.choiceIdList = choiceId;
+	}
+	
+	
 }
