4dad9970947fb5bed710a9ef614114cfa8eb4591
alvis <347390728@qq.com>
2016-07-14 09:26:41 +0800

    model definition finished

--- src/org/dclab/model/JudgementBean.java
+++ src/org/dclab/model/JudgementBean.java
@@ -1,5 +1,45 @@
 package org.dclab.model;
 
-public class JudgementBean {
+import java.io.Serializable;
 
+/**
+ * 判断题对象：
+ * 1. 题干ID, 内容 ；
+ * 2. 是否需要检查；
+ * 3. 考生答案id。
+ * @author alvis
+ *
+ */
+public class JudgementBean implements Serializable{
+	private int id;//题目id
+	private String content;//题目内容
+	private boolean ifCheck;//是否需要检查
+	private int choiceId;//考生答案id
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public boolean isIfCheck() {
+		return ifCheck;
+	}
+	public void setIfCheck(boolean ifCheck) {
+		this.ifCheck = ifCheck;
+	}
+	public int getChoiceId() {
+		return choiceId;
+	}
+	public void setChoiceId(int choiceId) {
+		this.choiceId = choiceId;
+	}
+	
+	
 }
--- src/org/dclab/model/SingleChoiceBean.java
+++ src/org/dclab/model/SingleChoiceBean.java
@@ -1,5 +1,7 @@
 package org.dclab.model;
 
+import java.io.Serializable;
+
 /**
  * 单选题对象：
  * 1. 题干ID, 内容 (参考数据库表)
@@ -8,6 +10,38 @@ package org.dclab.model;
  * @author alvis
  *
  */
-public class SingleChoiceBean {
 
+public class SingleChoiceBean implements Serializable{
+
+	private int id;//题目id
+	private String content;//题目内容
+	private boolean ifCheck;//是否需要检查
+	private int choiceId;//考生答案id
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public boolean isIfCheck() {
+		return ifCheck;
+	}
+	public void setIfCheck(boolean ifCheck) {
+		this.ifCheck = ifCheck;
+	}
+	public int getChoiceId() {
+		return choiceId;
+	}
+	public void setChoiceId(int choiceId) {
+		this.choiceId = choiceId;
+	}
+	
+	
 }
