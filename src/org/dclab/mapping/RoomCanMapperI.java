2da338572ef92b074bfbd48eafdc8e8d5b0095ca
alvis <347390728@qq.com>
2016-08-08 12:00:45 +0800

    modefy mapping after database changed

--- src/org/dclab/mapping/RoomCanMapperI.java
+++ src/org/dclab/mapping/RoomCanMapperI.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 import org.dclab.model.CandidateBean;
+import org.dclab.model.CheckBean;
 
 /**
  * @author alvis
@@ -14,10 +15,13 @@ public interface RoomCanMapperI {
 	@Select("select roomId from room_candidate where candidateId=#{Uid}")
 	public int getRoomIdByUid(int Uid);
 	
-	@Select("select seatNum,Uname,gender,user.Uid,Cid from room_candidate inner join user on room_candidate.candidateId=user.Uid where roomId=#{roomId}")
-	public List<CandidateBean> getUserListByRoomId(int roomId);
+	@Select("select seatNum,Uname,gender,user.Uid,Cid from room_candidate inner join user on room_candidate.candidateId=user.Uid where roomId=#{id} AND Uid=#{status}")//根据roomid和uid获取candidate
+	public CandidateBean getUserByRoomId(CheckBean checkbean);
 	
-	@Select("SELECT seatNum FROM room_candidate WHERE roomId=#{roomId} AND candidateId IS NULL ")
+	@Select("SELECT seatNum FROM room_candidate WHERE roomId=#{roomId} AND candidateId IS NULL ")//查询考场内的空座位
 	public List<Integer> getFreeSeatByRoomId(int roomId);
+	
+	@Select("SELECT candidateId FROM room_candidate WHERE roomId=#{roomId} AND candidateId IS NOT NULL")//查询考场的考生list
+	public List<Integer> getUidLIstByRoomId(int roomId);
 }
 
