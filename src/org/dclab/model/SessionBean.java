336cc6c06ee15e2db78505a1686494ae73795bc4
emilio66 <1005874540@qq.com>
2016-08-09 20:42:28 +0800

    complete room distribution service, allow more sessions

---
+++ src/org/dclab/model/SessionBean.java
@@ -0,0 +1,29 @@
+package org.dclab.model;
+
+import java.sql.Timestamp;
+
+public class SessionBean {
+	private int id;
+	private int duration;
+	private Timestamp startTime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public Timestamp getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Timestamp startTime) {
+		this.startTime = startTime;
+	}
+	
+	
+}
