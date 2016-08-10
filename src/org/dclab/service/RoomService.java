773ee9605a426473fe89f4b7d0d3825e8d85bff4
emilio66 <1005874540@qq.com>
2016-08-10 16:22:59 +0800

    add change session & exam room arrangement

--- src/org/dclab/service/RoomService.java
+++ src/org/dclab/service/RoomService.java
@@ -3,6 +3,7 @@
  */
 package org.dclab.service;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +12,13 @@ import org.dclab.model.RoomBean;
 import org.dclab.model.SessionBean;
 
 /**
- * Room distribution service 1. Get all students registered to this course 2.
- * Count how many session needed for this course by comparing students number
- * with seats number (Check the available room first) 3. Distribute the seats to
- * students
+ * Room distribution service 
+ * 座位分配流程：
+ *  1. 选择科目，得到考生数目 
+ *  2. 选择考试在哪一天举行，拿到这一天所有考场的考试安排情况，得到空闲座位数（原则上一天只安排一个科目的考试） 
+ *  3， 根据空闲座位数计算 session（场次）数目
+ *  4. 安排每个session 的考试时间 
+ *  5. 根据session将空闲座位分配给考生
  * 
  * @author zhaoz
  *
@@ -22,8 +26,26 @@ import org.dclab.model.SessionBean;
 public class RoomService {
 
 	public static boolean isFull = false;
-	public static int reservedSeatNum = 0; // TODO: load reserved seat number from `ems.properties` file
+	public static int reservedSeatNum = 2; // TODO: load reserved seat number
+											// from `ems.properties` file or DB
 
+	public int getStudentNum(int subjectId) {
+		int studentNum = 123; // TODO: get student number from `student_subject`
+								// table by subject Id
+		return studentNum;
+	}
+
+	public int getVacantSeats(long startDay) {
+		int vacantSeatNum = 125; // TODO: get vacant seats in this day
+
+		return vacantSeatNum;
+	}
+
+	public int getVacantRooms(long startDay) {
+		int vacantRoomNum = 5; // TODO: get vacant room number in this day
+
+		return vacantRoomNum;
+	}
 
 	/**
 	 * count how many session are needed for this subject
@@ -31,13 +53,14 @@ public class RoomService {
 	 * @param subjectId
 	 * @return
 	 */
-	public int getSessionNum(int subjectId) {
+	public int getSessionNum(int studentNum, int seatNum, int roomNum) {
 		int sessionNum = 1;
 
-		int seatNum = 123; 		// TODO: load SUM of room size from `room` table
-		int roomNum = 5; 		// TODO: load COUNT of room from `room` table
-		int studentNum = 223; 	// TODO: load COUNT of student from `candidate_subject` table
-		
+		// int seatNum = 123; // TODO: load SUM of room size from `room` table
+		// int roomNum = 5; // TODO: load COUNT of room from `room` table
+		// int studentNum = 223; // TODO: load COUNT of student from
+		// `candidate_subject` table
+
 		if (studentNum > seatNum) {
 			sessionNum = studentNum / seatNum;
 			int remainder = studentNum % seatNum;
@@ -55,17 +78,25 @@ public class RoomService {
 
 		return sessionNum;
 	}
-	
+
 	/**
-	 * save sessions to DB
+	 * save session-room relation to DB
 	 * 
 	 * @param sessionBeans
 	 */
-	public void saveSessions(List<SessionBean> sessionBeans) {
-		
+	public boolean saveSession(int subjectId, Timestamp startTime, int duration, int roomId) {
+		// TODO: firstly, load info from DB to check whether this room is vacant
+		// in this time period
+		boolean isVacant = true;
+
+		// TODO: if room is available, save their relation to DB
+
+		return isVacant;
 	}
 
 	/**
+	 * After binding session to room, retrieve those rooms and be prepared to
+	 * distribute seats to students
 	 * 
 	 * @param startTimestamp
 	 * @param duration
@@ -75,7 +106,8 @@ public class RoomService {
 		// TODO: create a `session_room` relation table, get all available room
 		// "select * from `session_room` where startTimestamp + duration <
 		// startTime || startTime + table.duration < startTimestamp"
-		List<RoomBean> availableRooms = new ArrayList<RoomBean>(); //TODO: load from DB
+		List<RoomBean> availableRooms = new ArrayList<RoomBean>(); // TODO: load
+																	// from DB
 
 		return availableRooms;
 	}
@@ -108,15 +140,69 @@ public class RoomService {
 				int roomId = roomBean.getId();
 
 				for (int i = 0; i < size - reservedSeatNum; i++) {
-					students.get(curPos).setRoomId(roomId); // set seat for each student
+					students.get(curPos).setRoomId(roomId); // set seat for each
+															// student
 					students.get(curPos).setSeatNum(i);
 					curPos++;
 				}
 			}
 		}
-		
-		// TODO: write data to DB, flush "students" list to table `room_candidate`
 
+		// TODO: write data to DB, flush "students" list to table
+		// `room_candidate`
+
+	}
+
+	/**
+	 * 
+	 * @param subjectId
+	 * @return
+	 */
+	public List<SessionBean> getAvailableSessions(int subjectId) {
+		long currentTime = System.currentTimeMillis();
+		// TODO: get all session behind this session ( startTime > currentTime)
+
+		return new ArrayList<SessionBean>();
+	}
+
+	/**
+	 * Change exam session for a candidate
+	 * 1. get subsequent session of this subject
+	 * 2. change seat
+	 * @param candidateId
+	 * @param subjectId
+	 */
+	public boolean changeSession(int candidateId, int sessionId){
+		
+		boolean isAssigned = false;
+		
+		//TODO: get all rooms belong to this session
+		List<RoomBean> roomBeans = new ArrayList<>();
+		int roomId = 0;
+		int seatId = 0;
+		for (RoomBean roomBean : roomBeans) {
+			roomId = roomBean.getId();
+			int size   = roomBean.getSize();
+			
+			//TODO: get student number in this room by roomId
+			int studentNum = 0;
+			//assign a vacant seat to this student 
+			if(size > studentNum){
+				seatId = studentNum;
+				isAssigned = true;	
+				break;
+			}
+		}
+		
+
+		if(isAssigned){
+			//TODO: save candiateId-sessionId-roomId-seatId to DB
+			
+			return true;
+		} else{
+			System.err.println("No Vacant Seat");
+			return false;
+		}
 	}
 
 }
