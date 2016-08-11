b90e9255ee863a21ff398ab0d57238b6933012dd
swy <704355529@qq.com>
2016-08-11 17:46:33 +0800

    manager-match/simple+alert -si

--- WebContent/newjs/examManage.js
+++ WebContent/newjs/examManage.js
@@ -1,5 +1,5 @@
 var examManage = angular.module('examManage', ['ui.bootstrap']);
-examManage.controller('examManagerCtrl', function ($scope, $http,$window) {
+examManage.controller('examManagerCtrl', function ($scope, $http, $window) {
     $scope.isCollapsed = false;
 
 
@@ -69,89 +69,89 @@ examManage.controller('examManagerCtrl', function ($scope, $http,$window) {
 
 });
 
-examManage.controller('singleCtrl', function ($scope, $http,$window) {
-    $scope.itemMessage = ['','','',''];
+examManage.controller('singleCtrl', function ($scope, $http, $window) {
+    $scope.itemMessage = ['', '', '', ''];
     $scope.rightAnswer = [];
 
     $scope.save = function () {
         // alert($scope.content + $scope.rightAnswer+$scope.itemMessage);
-        var choice={};
-        for(x in $scope.itemMessage){
-            choice[x*1+1]=$scope.itemMessage[x];
+        var choice = {};
+        for (x in $scope.itemMessage) {
+            choice[x * 1 + 1] = $scope.itemMessage[x];
         }
         // console.log(choice);
         // console.log($scope.rightAnswer);
         $http({
-			method: 'GET',
-			url: '/EMS/admin/addTopic',
-			params: {
-				token: $window.sessionStorage.stoken,
-                typeId:0,
-                content:$scope.content,
-                choice:choice,
-                List:$scope.rightAnswer
+            method: 'GET',
+            url: '/EMS/admin/addTopic',
+            params: {
+                token: $window.sessionStorage.stoken,
+                typeId: 0,
+                content: $scope.content,
+                choice: choice,
+                List: $scope.rightAnswer
 
-			}
-		}).then(function success(response) {
-               alert("保存成功！");
+            }
+        }).then(function success(response) {
+            alert("保存成功！");
         })
 
     }
     $scope.delete = function () {
-        $scope.itemMessage = ['','','',''];
+        $scope.itemMessage = ['', '', '', ''];
         $scope.rightAnswer = [];
-        $scope.content=[];
+        $scope.content = [];
 
     }
 
 
 });
-examManage.controller('judgeCtrl', function ($scope, $http,$window) {
-    $scope.itemMessage = ['',''];
+examManage.controller('judgeCtrl', function ($scope, $http, $window) {
+    $scope.itemMessage = ['', ''];
     $scope.rightAnswer = [];
 
     $scope.save = function () {
         // alert($scope.content + $scope.rightAnswer+$scope.itemMessage);
-        var choice={};
-        for(x in $scope.itemMessage){
-            choice[x*1+1]=$scope.itemMessage[x];
+        var choice = {};
+        for (x in $scope.itemMessage) {
+            choice[x * 1 + 1] = $scope.itemMessage[x];
         }
         $http({
-			method: 'GET',
-			url: '/EMS/admin/addTopic',
-			params: {
-				token: $window.sessionStorage.stoken,
-                typeId:2,
-                content:$scope.content,
-                choice:choice,
-                List:$scope.rightAnswer
+            method: 'GET',
+            url: '/EMS/admin/addTopic',
+            params: {
+                token: $window.sessionStorage.stoken,
+                typeId: 2,
+                content: $scope.content,
+                choice: choice,
+                List: $scope.rightAnswer
 
-			}
-		}).then(function success(response) {
-               alert("保存成功！");
+            }
+        }).then(function success(response) {
+            alert("保存成功！");
         })
 
     }
     $scope.delete = function () {
-        $scope.itemMessage = ['',''];
+        $scope.itemMessage = ['', ''];
         $scope.rightAnswer = [];
-        $scope.content=[];
+        $scope.content = [];
 
     }
 
 
 });
-examManage.controller('multipleCtrl', function ($scope, $http,$window) {
+examManage.controller('multipleCtrl', function ($scope, $http, $window) {
     $scope.right = [];
-    $scope.itemMessage = ['','','',''];
-    var List=[];
+    $scope.itemMessage = ['', '', '', ''];
+    var List = [];
     $scope.save = function () {
         // alert($scope.content + $scope.right+$scope.itemMessage);
-        var choice={};
-        for(x in $scope.itemMessage){
-            choice[x]=$scope.itemMessage[x];
-            if($scope.right[x]){
-               List.push(x);
+        var choice = {};
+        for (x in $scope.itemMessage) {
+            choice[x] = $scope.itemMessage[x];
+            if ($scope.right[x]) {
+                List.push(x);
             }
 
         }
@@ -159,139 +159,143 @@ examManage.controller('multipleCtrl', function ($scope, $http,$window) {
         // console.log(choice);
         // console.log(List);
         $http({
-			method: 'GET',
-			url: '/EMS/admin/addTopic',
-			params: {
-				token: $window.sessionStorage.stoken,
-                typeId:1,
-                content:$scope.content,
-                choice:choice,
-                List:List
+            method: 'GET',
+            url: '/EMS/admin/addTopic',
+            params: {
+                token: $window.sessionStorage.stoken,
+                typeId: 1,
+                content: $scope.content,
+                choice: choice,
+                List: List
 
-			}
-		}).then(function success(response) {
-               alert("保存成功！");
+            }
+        }).then(function success(response) {
+            alert("保存成功！");
         })
 
     }
     $scope.delete = function () {
-        $scope.itemMessage = ['','','',''];
+        $scope.itemMessage = ['', '', '', ''];
         $scope.right = [];
-        $scope.content=[];
+        $scope.content = [];
 
     }
 
 
 });
 
-examManage.controller('mpCtrl', function ($scope, $http,$window) {
-    $scope.itemMessage = [''];
-    $scope.answerMessage = [''];
-    // $scope.showMessage = function () {
-    //     // console.log('stem: ' + $scope.stem);
-    //     for (x in $scope.itemMessage) {
-    //         console.log($scope.itemMessage[x] + ' ' + $scope.answerMessage[x]);
-    //     }
-    // }
+examManage.controller('mpCtrl', function ($scope, $http, $window) {
+    $scope.itemMessage = ['', ''];
+    $scope.answerMessage = ['', ''];
+    var choice={};
     $scope.save = function () {
-        $http({
-			method: 'GET',
-			url: '/EMS/admin/addTopic',
-			params: {
-				token: $window.sessionStorage.stoken,
-                typeId:4,
-                content:$scope.content
+        for (x in $scope.itemMessage) {
+            choice[x] = $scope.answerMessage[x];
+        }
 
-			}
-		}).then(function success(response) {
-               alert("保存成功！");
+        $http({
+            method: 'GET',
+            url: '/EMS/admin/addTopic',
+            params: {
+                token: $window.sessionStorage.stoken,
+                typeId: 3,
+                content: $scope.content,
+                item: $scope.itemMessage,
+                List: $scope.answerMessage,
+                choice:choice
+
+            }
+        }).then(function success(response) {
+            alert("保存成功！");
         })
 
     }
     $scope.delete = function () {
-        $scope.content=[];
+        $scope.itemMessage = ['', ''];
+        $scope.answerMessage = ['', ''];
+        $scope.content = [];
 
     }
 });
 
-examManage.controller('mpCtrl', function ($scope, $http,$window) {
+examManage.controller('simpleCtrl', function ($scope, $http, $window) {
 
     $scope.save = function () {
         $http({
-			method: 'GET',
-			url: '/EMS/admin/addTopic',
-			params: {
-				token: $window.sessionStorage.stoken,
-                typeId:4,
-                content:$scope.content
+            method: 'GET',
+            url: '/EMS/admin/addTopic',
+            params: {
+                token: $window.sessionStorage.stoken,
+                typeId: 4,
+                content: $scope.content
 
-			}
-		}).then(function success(response) {
-               alert("保存成功！");
+            }
+        }).then(function success(response) {
+            alert("保存成功！");
         })
 
     }
     $scope.delete = function () {
-        $scope.content=[];
+        $scope.content = [];
 
     }
 });
 
-examManage.controller('subjectCtrl', function ($scope, $http,$window) {
+examManage.controller('subjectCtrl', function ($scope, $http, $window) {
     // alert($scope)
     $scope.save = function () {
         // alert($scope.content + $scope.right+$scope.itemMessage);
-        var choice={};
-        if($scope.singleCheckbox){
-            choice[0]=$scope.singleScore;
+        var choice = {};
+        if ($scope.singleCheckbox) {
+            choice[0] = $scope.singleScore;
         }
-        if($scope.multipleCheckbox){
-            choice[1]=$scope.multiScore+','+$scope.multifulScore;
+        if ($scope.multipleCheckbox) {
+            choice[1] = $scope.multiScore + ',' + $scope.multifulScore;
         }
-        if($scope.judgeCheckbox){
-            choice[2]=$scope.judgeScore;
+        if ($scope.judgeCheckbox) {
+            choice[2] = $scope.judgeScore;
         }
-        if($scope.matchCheckbox){
-            choice[3]=$scope.matchScore;
+        if ($scope.matchCheckbox) {
+            choice[3] = $scope.matchScore;
         }
-        if($scope.simpleCheckbox){
-            choice[4]=$scope.simpleScore;
+        if ($scope.simpleCheckbox) {
+            choice[4] = $scope.simpleScore;
         }
         // console.log(choice);
         // alert(List+$scope.itemMessage)
         $http({
-			method: 'GET',
-			url: '/EMS/admin/addSubject',
-			params: {
-				token: $window.sessionStorage.stoken,
-                name:$scope.name,
-                duration:$scope.duration,
-                earliestSubmit:$scope.earliestSubmit,
-                latestLogin:$scope.latestLogin,
-                map:choice
+            method: 'GET',
+            url: '/EMS/admin/addSubject',
+            params: {
+                token: $window.sessionStorage.stoken,
+                name: $scope.name,
+                duration: $scope.duration,
+                earliestSubmit: $scope.earliestSubmit,
+                latestLogin: $scope.latestLogin,
+                map: choice
 
-			}
-		}).then(function success(response) {
-               alert("保存成功！");
+            }
+        }).then(function success(response) {
+            alert("保存成功！");
         })
 
     }
     $scope.delete = function () {
         $scope.name = [];
         $scope.duration = [];
-        $scope.earliestSubmit=[];
-        $scope.latestLogin=[];
-        $scope.singleCheckbox=false;
-        $scope.singleScore=[];
-        $scope.multipleCheckbox=false;
-        $scope.multifulScore=[];
-        $scope.multiScore=[];
-        $scope.judgeCheckbox=false;
-        $scope.judgeScore=[];
-        $scope.matchCheckbox=false;
-        $scope.matchScore=[];
-        $scope.simpleCheckbox=false;
-        $scope.simpleScore=[];
+        $scope.earliestSubmit = [];
+        $scope.latestLogin = [];
+        $scope.singleCheckbox = false;
+        $scope.singleScore = [];
+        $scope.multipleCheckbox = false;
+        $scope.multifulScore = [];
+        $scope.multiScore = [];
+        $scope.judgeCheckbox = false;
+        $scope.judgeScore = [];
+        $scope.matchCheckbox = false;
+        $scope.matchScore = [];
+        $scope.simpleCheckbox = false;
+        $scope.simpleScore = [];
 
     }
 });
