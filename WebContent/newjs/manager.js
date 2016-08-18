af4e6abcac3d3c52e19586aea72fac3178953215
swy <704355529@qq.com>
2016-08-18 22:31:58 +0800

    manager/export -si

--- WebContent/newjs/manager.js
+++ WebContent/newjs/manager.js
@@ -5,8 +5,7 @@ angular.module('manager').controller('managerCtrl', function ($rootScope, $scope
     var adminStatus = JSON.parse($window.sessionStorage.adminStatus);
     // 功能列表
     $scope.operationMetaInfo = adminStatus.authorityList;
-    //初始化表格隐藏
-    $scope.showRoom = "none";
+
     //侧边栏左
     $scope.operationMetaInfo = [
         { "name": "导入试卷", "url": "importExam" },
@@ -27,22 +26,8 @@ angular.module('manager').controller('managerCtrl', function ($rootScope, $scope
     // 初始化显示标签0
     $scope.index = 0;
 
-    
-
     $rootScope.role = "管理员";
-    //初始化表格
-    $scope.roomMetaInfo = {
-        'id': '场次',
-        'roomName': '考场名',
-        'startTime': '开考时间',
-        'size': '考场人数',
-        'status': '状态'
-    };
-    // 状态码转化成易读string
-    $scope.statusDisplay = ['未登录', '已登录'];
-    $scope.roomsStatus = {};
-    $scope.selectionStatus = {};
-
+   
 
     // 监控index变量控制标签及标签页
     $scope.$watch('index', function () {
@@ -69,17 +54,33 @@ angular.module('manager').controller('managerCtrl', function ($rootScope, $scope
                 break;
 
             case "importExam": // 导入试卷
-                $scope.showRoom = "none";
+                // $scope.showRoom = "none";
                 break;
             case "importStuArrangement": // 导入考生安排
-                $scope.showRoom = "none";
+                // $scope.showRoom = "none";
                 break;
             case "roomArrangement": // 考场管理
-                $scope.showRoom = "block";
+                // $scope.showRoom = "block";
                 $scope.selectionStatus = {};
                 break;
             case "exportExam": // 成绩导出
-                $scope.showRoom = "none";
+                // $scope.showRoom = "none";
+                $scope.exportByRoom = [{
+                    id: 1
+                }, { id: 31 }, { id: 11 }, { id: 14 }]
+
+                //请求场次列表/sumList在父域中赋值
+                $http.get('/EMS/admin/roomLists', {
+                    // $http.get('info.json', {
+                    params: {
+                        token: $window.sessionStorage.stoken
+                    }
+                }).then(function successCallback(response) {
+                    $scope.exportByRoom = response.data;
+                    $scope.orderCondition = 'id';
+			        $scope.isReverse = false;
+                }, function errorCallback(response) {
+                });
                 break;
             case "systemManagement": // 系统管理
                 $scope.showRoom = "none";
@@ -90,6 +91,30 @@ angular.module('manager').controller('managerCtrl', function ($rootScope, $scope
         }
 
     };
+
+});
+angular.module('manager').controller('roomCtrl', function ($rootScope, $scope, $http, $window, $state, $interval) {
+
+    //侧边栏左
+    $scope.operationMetaInfo = [
+        { "name": "导入试卷", "url": "importExam" },
+        { "name": "导入考生安排", "url": "importStuArrangement" },
+        { "name": "考场管理", "url": "roomArrangement" },
+        { "name": "成绩导出", "url": "exportExam" },
+        { "name": "系统管理", "url": "systemManagement" }];
+
+    //初始化表格
+    $scope.roomMetaInfo = {
+        'id': '场次',
+        'roomName': '考场名',
+        'startTime': '开考时间',
+        'size': '考场人数',
+        'status': '状态'
+    };
+    // 状态码转化成易读string
+    $scope.statusDisplay = ['未登录', '已登录'];
+    $scope.roomsStatus = {};
+    $scope.selectionStatus = {};
 
     $scope.confirm = function (url) {
 
@@ -200,6 +225,8 @@ angular.module('manager').controller('managerCtrl', function ($rootScope, $scope
                     }
                 }
                 $scope.cancelAll();
+                $scope.orderCondition = 'id';
+			    $scope.isReverse = false;
             },
             function error(response) {
                 alert('刷新出错\n' + response.status
@@ -252,7 +279,7 @@ angular.module('manager').controller('managerCtrl', function ($rootScope, $scope
                                 alert('考生状态错误！');
                         }
                     }
-                    $scope.orderCondition = 'roomId';
+                    $scope.orderCondition = 'id';
                     $scope.isReverse = false;
                 },
                 function error(response) {
@@ -284,7 +311,7 @@ angular.module('manager').controller('ImportFile', function ($rootScope, $scope,
         $scope.progressPer = 0;
         $http({
             method: 'POST',
-            url: 'examForm',
+            url: '/EMS/admin/examForm',
             data: formData,
             headers: {
                 'Content-Type': undefined,
@@ -319,7 +346,7 @@ angular.module('manager').controller('ImportStuFile', function ($rootScope, $sco
         $scope.ngshow = true;
         var formData = new FormData();
         formData.append("file", $scope.selectedFile);
-        formData.append("token",$window.sessionStorage.stoken);
+        formData.append("token", $window.sessionStorage.stoken);
         if ($scope.selectedFile == undefined) {
             return;
         }
@@ -347,11 +374,117 @@ angular.module('manager').controller('ImportStuFile', function ($rootScope, $sco
 
 });
 angular.module('manager').controller('exportFile', function ($rootScope, $scope, $http, $window, $state, $interval) {
+    //初始化表格
+    $scope.roomMetaInfo = {
+        'id': '场次'
+    };
+    // 状态码转化成易读string
+    // $scope.statusDisplay = ['未登录', '已登录'];
+    // $scope.roomsStatus = {};
+    $scope.selectionStatus = {};
+    // $scope.exportByRoom=[{
+    //     id:1
+    // },{id:3},{id:11},{id:14}]
+
+    // 全选
+    $scope.selectAll = function () {
+        for (x in $scope.exportByRoom) {
+            $scope.selectionStatus[$scope.exportByRoom[x].id] = true;
+        }
+    }
+
+    // 取消选择
+    $scope.cancelAll = function () {
+        for (x in $scope.exportByRoom) {
+            $scope.selectionStatus[$scope.exportByRoom[x].id] = false;
+        }
+    }
+
+    // 单独选择
+    $scope.checkSel = function (status, roomId) {
+        $scope.cancelAll();
+        $scope.selectionStatus[roomId] = true;
+        if (status == true) {
+            $scope.selectedNum += 1;
+        }
+        if (status == false) {
+            $scope.selectedNum -= 1;
+        }
+    }
+
+    // 排序变量
+    $scope.thClick = function (value) {
+        $scope.orderCondition = value;
+        $scope.isReverse = !$scope.isReverse;
+    }
+
+    // 刷新
+    $scope.refresh = function () {
+        // refresh();
+    };
+    //每间隔30s自动刷新
+    var timingPromise = undefined;
+    timingPromise = $interval(function () { refresh() }, 30000);
+
+    function refresh() {
+
+        $http({
+            method: 'GET',
+            url: '/EMS/admin/Refresh',
+            params: {
+                token: $window.sessionStorage.stoken
+            }
+        })
+            .then(
+            function success(response) {
+                $scope.exportByRoom = response.data;
+                $scope.absentNum = 0;
+                $scope.examingNum = 0;
+                for (x in $scope.exportByRoom) {
+                    switch ($scope.exportByRoom[x].status) {
+                        case 0:
+                            $scope.absentNum += 1;
+                            $scope.roomsStatus[$scope.exportByRoom[x].roomId] = '';
+                            break;
+                        case 1:
+                            $scope.examingNum += 1;
+                            $scope.roomsStatus[$scope.exportByRoom[x].roomId] = 'info';
+                            break;
+                        default:
+                            alert('考生状态错误！');
+                    }
+                }
+                $scope.cancelAll();
+                $scope.orderCondition = 'id';
+			    $scope.isReverse = false;
+            },
+            function error(response) {
+                alert('刷新出错\n' + response.status
+                    + ' ' + response.statusText);
+            });
+    }
+
+
     $scope.sumExport = function () {
-        window.open("/EMS/admin/sumDownload?token="+$window.sessionStorage.stoken);
+        var uidList = [];
+        for (x in $scope.selectionStatus) {
+
+            if ($scope.selectionStatus[x]) {
+                uidList.push(x);
+            }
+        }
+        alert(uidList);
+        window.open("/EMS/admin/sumDownload?token=" + $window.sessionStorage.stoken);
     };
     $scope.stuExport = function () {
-        window.open("/EMS/admin/stuDownload?token="+$window.sessionStorage.stoken);
+        var uidList = [];
+        for (x in $scope.selectionStatus) {
+
+            if ($scope.selectionStatus[x]) {
+                uidList.push(x);
+            }
+        }
+        window.open("/EMS/admin/stuDownload?token=" + $window.sessionStorage.stoken);
     }
 
 
